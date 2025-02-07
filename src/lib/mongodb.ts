499d9e73a3bfbd83d0c43a8562722698e9c87208
Ali Sulman <alithedeveloper1470@gmail.com>
2025-02-08 00:06:36 +0500

    update vecel error

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { Mongoose } from "mongoose";
 
-let cached: { conn: Mongoose | null; promise: Promise<Mongoose> | null} = {
+const cached: { conn: Mongoose | null; promise: Promise<Mongoose> | null} = {
     conn: null,
     promise: null,
 }
